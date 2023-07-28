0c24d966534c7673f34c0db9d0486e68cc1d06f6
Travis Huynh <118507351+DuhBoss32@users.noreply.github.com>
2023-07-28 16:31:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.navbar');
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >= 800) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
